d34fde4eb41adf177512d32362ed0f6d1b6d59ea
lenabatik <lena-batik@mail.ru>
2015-11-27 18:29:40 +0600

    ispravila main.py po zame4anijam

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import unittest
 from unittest import TestCase
 from selenium import webdriver
 
-
 class Status_Button_Disabled_Test(TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Firefox() #создание класса
+        self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(5)
         self.driver.get('https://www.onetwotrip.com')
 
     def test_status_button(self):
-        # проверка невозможности выполнения запроса при некоторых пустых полях
+        """ проверка невозможности выполнения запроса при некоторых пустых полях
 
-        # заполняем два поля "откуда" и "куда"
-        # поле "когда" оставляем пустым
-        # и проверяем статус кнопки "Найти"
-        # надо, чтобы статус кнопки при этом был disabled
-
+        заполняем два поля "откуда" и "куда"
+        поле "когда" оставляем пустым
+        и проверяем статус кнопки "Найти"
+        надо, чтобы статус кнопки при этом был disabled
+		"""
+		
         field_from = self.driver.find_element_by_name('from0')
         field_from.send_keys('Новосибирск')
 
@@ -26,10 +26,7 @@ class Status_Button_Disabled_Test(TestCase):
         field_to.send_keys('Москва')
 
         button_status = self.driver.find_element_by_name('submit')
-        text_button_status = button_status.get_attribute('class')
-
-        self.assertEqual("disabled", text_button_status)
-
+		self.assertFalse(button_status.is_enabled())
 
     def tearDown(self):
         self.driver.quit()
