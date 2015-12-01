385cda321c36c0db0659e10a89db2a41fc6b9a86
Foootbaler <Student-prk@mail.ru>
2015-12-01 20:24:13 +0600

    New update test_feedly.py
    Добавлено описание тест-кейса, изменен метод кнопки search_field

--- test_feedly.py
+++ test_feedly.py
@@ -16,6 +16,17 @@ class AutomaticTestCase(TestCase):
         self.driver.get('http://feedly.com/')
         
     def AddNewsInFeedly(self):
+        """Предусловия
+        Для тестирования необходима учетная запись на сайте feedly.com.
+        1. Перейти на сайт feedly.com, войти в учетную запись, нажав кнопку "Get started", выбрав способ входа и введя корректный логин и пароль.
+        После проведения данных действий вы будете перенаправлены на страницу https://feedly.com/i/discover
+        Шаги воспроизведения
+        1. В строку поиска ввести, например, "sportbox.ru" и нажать кнопку "Enter"
+        2. Удостовериться, что появился элемент "Sportbox.ru - Главные новости" и нажать кнопку "+"
+        3. В окне слева нажать кнопку "Add"
+        Результат
+        В результате в окне контента, расположенном слева, появится вкладка "Sportbox.ru", в которой будет отображаться "Sportbox.ru - Главные новости"
+        """
         self.driver.find_element_by_class_name('primary').click()
         handles = self.driver.window_handles
         self.driver.switch_to.window(handles[1])
@@ -32,7 +43,7 @@ class AutomaticTestCase(TestCase):
         self.driver.switch_to.window(handles[0])
         search_field = self.driver.find_element_by_id('maxHerculeInput')
         search_field.send_keys(SEARCH_DATA)
-        search_field.send_keys(Keys.RETURN)
+        search_field.submit()
 
         self.driver.find_element_by_class_name('simpleFollowButton').click()
         self.driver.find_element_by_id('subscribe').click()
