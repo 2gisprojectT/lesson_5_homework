c3971ea62b09b18a135175e06a950d16cc506ffd
pozdnysheva <nataliya666@gmail.com>
2015-12-01 23:22:10 +0600

    Create onetwotripTest.py

---
+++ onetwotripTest.py
@@ -0,0 +1,55 @@
+# coding: UTF-8
+from unittest import TestCase
+from selenium import webdriver
+import unittest
+import time
+
+
+class OnetwotripTestingBookingForm(TestCase):
+    """
+    Проверка формы бронирования на сайте http://www.onetwotrip.com
+    """
+    def setUp(self):
+        """
+        Предусловия:
+        Зайти на сайт http://www.onetwotrip.com,
+        выбрать параметры рейса (Заполнить поля "Откуда", "Куда", "Туда", "Обратно"),
+        выбрать некоторый рейс из предложенных.
+        """
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(20)
+        self.driver.get('http://www.onetwotrip.com')
+        self.driver.find_element_by_css_selector('#from0').send_keys('Москва')
+        self.driver.find_element_by_css_selector('#to0').send_keys('Минск')
+        self.driver.find_element_by_css_selector('#date0').click()
+        self.driver.find_element_by_css_selector('.calendar').click()
+        self.driver.find_element_by_css_selector('#date1').click()
+        self.driver.find_element_by_css_selector('.calendar').click()
+        self.driver.find_element_by_class_name('search').click()
+        self.driver.find_element_by_class_name('price_button').click()
+
+    def test_GoodValues(self):
+        """
+        Тест: Ввод верных значений в форму бронирования
+        Шаги:
+            1.Ввести корректный e-mail
+            2.Ввести корректныую фамилию
+            3.Ввести корректное имя
+            4.Ввести корректную дату рождения
+            5.Ввести корректный номер документа, если есть поле "Документ"
+            6.Нажать кнопку "Отправить"
+        Ожидаемый результат: переход на страницу "Оформление билета"
+        """
+        self.driver.find_element_by_css_selector('#input_avia_book_email').send_keys('test@test.ru')
+        self.driver.find_element_by_css_selector('#input_lastName0').send_keys('Pozdnyshev')
+        self.driver.find_element_by_css_selector('#input_firstName0').send_keys('Maxim')
+        self.driver.find_element_by_css_selector('#input_birthDate0').send_keys('25.04.1994')
+        # self.driver.find_element_by_css_selector('#input_passNumber0').send_keys('123456')
+        self.driver.find_element_by_class_name('submit').submit()
+        time.sleep(20)
+
+        text = self.driver.find_element_by_css_selector('#pageTitle').text
+        self.assertEqual('Оформление билета', text)
+
+    def tearDown(self):
+        self.driver.quit()
