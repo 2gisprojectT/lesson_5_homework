7ad94cb6e9285fbd74d9d57f1fca8733d26bd6f1
mxklsn <mxklsn@gmail.com>
2015-12-04 17:46:57 +0600

    ОТрефакторил код, сделал проще. ДОбавил коментирование, исправл ошибки

--- selenium-python_test.py
+++ selenium-python_test.py
@@ -16,17 +16,21 @@ class SeleniumTest(TestCase):
         self.driver.quit()
 
     def test_email(self):
-        """Functional test for input invalid email, click to send button and get error message."""
-        try:
-            WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "text-input-input")))
-        finally:
-            action = ActionChains(self.driver)
-            action.send_keys('max@')  # почта без доменной части
-            action.perform()
+        """Описание тест-кейса: https://projectt2015.testrail.net/index.php?/cases/edit/253/1
+            1. Название: Поле ввода содержит знак «@» и корректные знаки перед ним, но не содержит доменное имя, пробуем
+             авторизоваться
+            2. Предпосылки: зайти на сайт https://www.dropbox.com/login
+            3. Шаги:
+                - Ввести адрес электронной почты без доменного имени, например «max@»
+                - Поставить галочку «Запомнить», если это необходимо
+                - Нажать кнопку «Войти»
+            4. Ожидаемый результат: Получаем ошибку «Неверное название домена (часть адреса эл. почты после символа @:
+            ).»"""
+        self.element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "text-input-input"))).send_keys('max@')
 
-            self.driver.find_element_by_class_name('login-button').click()  # «Войти»
-            text = self.driver.find_element_by_class_name('error-message').text
+        self.driver.find_element_by_class_name('login-button').click()  # «Войти»
+        text = self.driver.find_element_by_class_name('error-message').text
 
-            self.assertIn(text, ['Неверное название домена (часть адреса эл. почты после символа @: ).',
-                                 'The domain portion of the email address is invalid (the portion after the @: )'])
+        self.assertIn(text, ['Неверное название домена (часть адреса эл. почты после символа @: ).',
+                             'The domain portion of the email address is invalid (the portion after the @: )'])
