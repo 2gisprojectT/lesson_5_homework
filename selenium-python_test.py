5277b46fad792aeb1f25122c5a0b198a02304d99
mxklsn <mxklsn@gmail.com>
2015-12-04 11:25:28 +0600

    Refactor code

--- selenium-python_test.py
+++ selenium-python_test.py
@@ -2,6 +2,9 @@
 from unittest import TestCase
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class SeleniumTest(TestCase):
@@ -13,12 +16,17 @@ class SeleniumTest(TestCase):
         self.driver.quit()
 
     def test_email(self):
-        action = ActionChains(self.driver)
-        self.driver.implicitly_wait(30)
-        action.send_keys('max@')  # почта без доменной части
-        action.perform()
-        self.driver.find_element_by_xpath('//*[@id="regular-login-forms"]/form[1]/div[4]/button').click()  # «Войти»
-        text = self.driver.find_element_by_class_name('error-message').text
+        """Functional test for input invalid email, click to send button and get error message."""
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "text-input-input")))
+        finally:
+            action = ActionChains(self.driver)
+            action.send_keys('max@')  # почта без доменной части
+            action.perform()
 
-        self.assertIn(text, ['Неверное название домена (часть адреса эл. почты после символа @: ).',
-                             'The domain portion of the email address is invalid (the portion after the @: )'])
+            self.driver.find_element_by_class_name('login-button').click()  # «Войти»
+            text = self.driver.find_element_by_class_name('error-message').text
+
+            self.assertIn(text, ['Неверное название домена (часть адреса эл. почты после символа @: ).',
+                                 'The domain portion of the email address is invalid (the portion after the @: )'])
